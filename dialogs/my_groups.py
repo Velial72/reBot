6bcbb278e822d2c49af0afb007274c2301377680
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-04-14 21:32:49 +0500

    upgrade 2.0

--- dialogs/my_groups.py
+++ dialogs/my_groups.py
@@ -6,8 +6,8 @@ from aiogram.enums import ContentType
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import User, CallbackQuery, Message
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
-from aiogram_dialog.widgets.text import Const, Format
-from aiogram_dialog.widgets.kbd import Button, ScrollingGroup, Next
+from aiogram_dialog.widgets.text import Const, Format, Multi
+from aiogram_dialog.widgets.kbd import Button, ScrollingGroup, Select, SwitchTo, Back
 from aiogram_dialog.widgets.input import TextInput, ManagedTextInput, MessageInput
 from aiogram_dialog.widgets.media import StaticMedia
 from aiogram.utils.deep_linking import create_start_link
@@ -18,7 +18,7 @@ import pprint
 
 group_data = {
     'xsf': {
-        'admin_id': 1,
+        'admin_id': True,
         'group_name': 'Умные Хомяки',
         'description': 'test',
         'price': '1000',
@@ -26,7 +26,7 @@ group_data = {
         'wish': 'car'
     },
     'sfd': {
-        'admin_id': 0,
+        'admin_id': False,
         'group_name': 'Горячие головы',
         'description': 'qwerty',
         'price': '5000',
@@ -34,7 +34,7 @@ group_data = {
         'wish': 'airplane'
     },
     'Yellow_tree': {
-        'admin_id': 0,
+        'admin_id': False,
         'group_name': 'Yellow tree',
         'description': 'Green tree',
         'price': '4500',
@@ -42,7 +42,7 @@ group_data = {
         'wish': 'airplane'
     },
     'Justice_league': {
-        'admin_id': 1,
+        'admin_id': True,
         'group_name': 'Justice league',
         'description': 'Я - Бэтмен',
         'price': '3000',
@@ -55,36 +55,61 @@ group_data = {
 class GroupSG(StatesGroup):
     start = State()
     chosen_group = State()
+    group_info = State()
+    admin_group = State()
 
 
-async def get_group_data(dialog_manager: DialogManager,**kwargs):
-    pprint.pprint(dialog_manager.start_data)
-    return {'group_name': 'test', 'description': 'test 2', 'link': 'test 3'}
-
-
-def group_btn_creator(btn):
+async def group_btn_creator(**kwargs):
     buttons = []
-    for group in btn.keys():
-        buttons.append(Next(Const(group), id=group))
-    return buttons
+    for group_id, group_name in group_data.items():
+        btn = (group_name['group_name'], group_id)
+        buttons.append(btn)
+    return {'buttons': buttons}
 
 
-group_btn = group_btn_creator(group_data)
+async def get_group_data(callback: CallbackQuery, button: Button,
+                         dialog_manager: DialogManager, item_id: str):
+    dialog_manager.dialog_data['group_name'] = item_id
+    await dialog_manager.next()
+
+
+async def get_group_name(dialog_manager: DialogManager, **kwargs):
+    group = dialog_manager.dialog_data['group_name']
+    return {
+        'group_name': group_data[group]['group_name'],
+        'description': group_data[group]['description'],
+        'link': group_data[group]['link'],
+        'admin_id': group_data[group]['admin_id']
+    }
+
 
 my_group_dialog = Dialog(
     Window(
         Const('Вот твои группы'),
         ScrollingGroup(
-            *group_btn,
-            id="my_groups",
-            width=1,
-            height=2
+            Select(
+                Format('{item[0]}'),
+                id='on_group',
+                item_id_getter=lambda x: x[1],
+                items='buttons',
+                on_click=get_group_data
+            ),
+            id='scroll_groups',
+            width=2,
+            height=1
         ),
-        state=GroupSG.start
+        state=GroupSG.start,
+        getter=group_btn_creator
     ),
     Window(
-        Format('Группа {group_name}\n{description}\nСсылка для приглашения участников: {link}'),
-        getter=get_group_data,
+        Format('Группа {group_name}\n{description}'),
+        Format('Ссылка для приглашения участников: {link}', when='admin_id'),
+        SwitchTo(Const('Управлять группой'), id='admin_group', state=GroupSG.admin_group, when='admin_id'),
+        Back(Const('Назад'),id='step_back'),
+        getter=get_group_name,
         state=GroupSG.chosen_group
-    )
+    ),
+    # Window(
+    #
+    # )
 )
